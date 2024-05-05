2a913375f1fc9447d892c3a877a9649f78406540
70109114 <70109114@uol.pk>
2024-05-05 16:44:35 +0500

    05/05/24

---
+++ index.js
@@ -0,0 +1,89 @@
+// var day = "sunday";
+// switch (day) {
+
+//     case 'Mnday':
+//         console.log("today is Monday");
+//         break;
+
+//     case 'Tuesday':
+//         console.log("today is Tuesday");
+//         break;
+
+//     case 'wednesday':
+//         console.log("today is wednesday");
+//         break;
+
+//     case 'thursday':
+//         console.log("today is thursday");
+//         break;
+
+//     case 'friday':
+//         console.log("today is friday");
+//         break;
+
+//     case 'saturday':
+//         console.log("today is saturday");
+//         break;
+
+//     case 'sunday':
+//         console.log("today is sunday");
+//         break;
+
+//     default:
+//         console.log("incorrect value");
+//         break;
+
+
+// }
+
+// loops and conditions statement:
+// for( i=1; i<3; i++){
+//     for(j=1; j<7; j++){
+//         console.log("week" +i+ "day" +j);
+//     }
+
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i == 1) {
+//         console.log("gold medal");
+//     }
+//     else if (i == 2) {
+//         console.log("silver medal");
+//     }
+//     else if (i == 3) {
+//         console.log("bronze medal");
+//     }
+//     else {
+//         console.log(i);
+//     }
+// }
+
+// functions:
+// function addTwoNums(a,b){
+//     var c = a + b;
+//     console.log(c);
+
+
+// }
+// for (var i = 0; i <10; i++) {
+//     addTwoNums(2,3);
+// }
+
+// Array:
+// var train1= ["wheat", "barley", "potato", "salt", "rocks"];
+// console.log(train1[0]);
+
+// Arrays, funcntion, loops:
+var train1= ["wheat", "barley", "potato", "salt", "rocks"];
+function array(){
+    console.log(train1)
+}
+
+for(var i=0; i<5; i++){
+    array();
+    break;
+}
+
+
+
